7b5cd90a3b10efd4faad3981937127b3309552bd
Adam Bodnar <abodnar@nxtboard.com>
2019-06-28 11:10:55 -0500

    Update to actually merge documents to show error

--- generate_pdf.php
+++ generate_pdf.php
@@ -23,10 +23,13 @@ class BoardPacket
 
         $this->documents = [
             257 => [
-                0 => ['title' => 'U3.2 1st Quarterly Progress Checker2.pdf', 'id' => 23],
-                1 => ['title' => 'CT_DesktopWallpaper-1.jpg', 'id' => 1],
-                2 => ['title' => '1c41e85330ee8032a95049d6bc6b58e8.jpg 11-55-40-729.pdf', 'id' => 28]
-            ]
+                0 => ['title' => '3xiX27gQcKdHqhhGXKqO-44616797.pdf', 'filename' => '3xiX27gQcKdHqhhGXKqO-44616797.pdf'],
+            ],
+            260 => [
+                0 => ['title' => '6LLkKP8HRcu10JpDqvrN-44479652.pdf', 'filename' => '6LLkKP8HRcu10JpDqvrN-44479652.pdf'],
+                1 => ['title' => '7O41edsOSmOFhCX2fQ2g-44605776.pdf', 'filename' => '7O41edsOSmOFhCX2fQ2g-44605776.pdf'],
+                2 => ['title' => 'TpVWxkKbTCyt2OLLtHxV-44588801.pdf', 'filename' => 'TpVWxkKbTCyt2OLLtHxV-44588801.pdf'],
+            ],
         ];
 
         $boardPacketFileName = 'Board_Packet.pdf';
@@ -59,7 +62,24 @@ class BoardPacket
         $document->setWriter($writer);
 
         $outlines = $document->getCatalog()->getOutlines();
-        $this->buildTableOfContents(0, $outlines, $document); // Start with the main Agenda
+
+        // Create a merger instance on this document. This is used to merge the attached files
+        $merger = new SetaPDF_Merger($document);
+
+        $this->buildTableOfContents(0, $outlines, $document, $merger); // Start with the main Agenda
+
+        // merge them together
+        try {
+            $merger->merge();
+        } catch (Throwable $e) {
+            error_log('Unable to merge the files together, failing.');
+            error_log($e->getMessage());
+
+            die();
+        }
+
+        // get the resulting document instance
+        $document = $merger->getDocument();
 
         $document->getCatalog()->setPageMode(SetaPDF_Core_Document_PageMode::USE_OUTLINES);
         $document->setWriter(new SetaPDF_Core_Writer_File(__DIR__ . '/' . $boardPacketFileName));
@@ -68,7 +88,7 @@ class BoardPacket
         $document->save()->finish();
     }
 
-    private function buildTableOfContents($index, $outlineItem, $document)
+    private function buildTableOfContents($index, $outlineItem, $document, $merger)
     {
         $item = SetaPDF_Core_Document_OutlinesItem::create($document, $this->tableOfContents[$index]['title']);
         $outlineItem->appendChild($item);
@@ -77,14 +97,20 @@ class BoardPacket
         if (isset($this->documents[$index])) {
             /** @var array $attachedDoc */
             foreach ($this->documents[$index] as $attachedDoc) {
-                $fileItem = SetaPDF_Core_Document_OutlinesItem::create($document, $attachedDoc['title']);
-                $item->appendChild($fileItem);
+                $merger->addFile([
+                    'filename' => __DIR__ . '/files/' . $attachedDoc['filename'],
+                    'outlinesConfig' => [
+                        SetaPDF_Merger::OUTLINES_TITLE => $attachedDoc['title'],
+                        SetaPDF_Merger::OUTLINES_PARENT => $item,
+                        SetaPDF_Merger::OUTLINES_COPY => SetaPDF_Merger::COPY_OUTLINES_AS_CHILDS,
+                    ],
+                ]);
             }
         }
 
         /** @var int $child */
         foreach ($this->tableOfContents[$index]['children'] as $child) {
-            $this->buildTableOfContents($child, $item, $document);
+            $this->buildTableOfContents($child, $item, $document, $merger);
         }
     }
 }
